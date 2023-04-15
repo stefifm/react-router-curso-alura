edb6ee768a7210a15ce64e653c18d599b93dcb8b
stefifm <bruerastefania@gmail.com>
2023-04-15 12:54:18 -0300

    Add list of categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import ListCategories from '../components/Categories/ListCategories'
 import ListPosts from '../components/ListPosts/ListPosts'
 
 function Home() {
@@ -6,6 +7,7 @@ function Home() {
       <div className='container'>
         <h2 className='title-page'>Pet noticias</h2>
       </div>
+      <ListCategories />
       <ListPosts url={'/posts'} />
     </main>
   )
